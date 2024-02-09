be2b290210e8de2ba182a1e42a17c1ac9a1ffcb5
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-02-09 15:35:23 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import apptest from './components/HelloWorld.vue';
+export default{
+  data(){
+    return{};
+  },
+  components:{apptest},
+};
+
+</script>
+
+<template>
+  <apptest/>
+</template>
+
+<style scoped>
+</style>
